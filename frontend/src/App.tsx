26d68509670047a8693a7804faa28a4a9a7e5597
nick <klockgether.nick@gmail.com>
2022-12-13 18:03:31 +0100

    Install axios, Create hook to get the accestoken, Create endpoint to get the accestoken

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Map from "./components/Map";
+import useAccessToken from "./useAccessToken";
 
 function App() {
+
+  const {token} = useAccessToken()
+
   return (
-      <Map/>
-  );
+      <Map token={token}/>
+  )
 }
 
 export default App;
