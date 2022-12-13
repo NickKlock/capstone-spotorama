65180090a9303a7199eada0ac7cfb9ddeed857da
nick <klockgether.nick@gmail.com>
2022-12-13 17:12:28 +0100

    Remove default app.css, Add mapbox to display

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,7 +1,42 @@
+import React, {MutableRefObject, useEffect, useRef, useState} from "react";
+import mapboxgl from 'mapbox-gl'
+import "./MapContainer.css"
+import {Box} from "@mui/material";
+
+mapboxgl.accessToken = "pk.eyJ1Ijoibmlja2tsb2NrIiwiYSI6ImNraXEzZDhqNDFzaWEyeXBrbm5sOTd4OTAifQ.1qnQdThQElj-xFUOc85aPQ"
 export default function Map(){
+    const mapContainer= useRef<HTMLDivElement| string>("") as MutableRefObject<HTMLDivElement>;
+    const map = useRef<mapboxgl.Map>();
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
 
-    return(<div>
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            attributionControl:true,
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/satellite-streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
 
-        </div>
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            if (map.current){
+                setLng(parseFloat(map.current?.getCenter().lng.toFixed(4)));
+                setLat(parseFloat(map.current?.getCenter().lat.toFixed(4)));
+                setZoom(parseFloat(map.current?.getZoom().toFixed(2)));
+            }
+
+        });
+    });
+
+    return(
+        <Box>
+            <Box ref={mapContainer} />
+        </Box>
     )
 }
