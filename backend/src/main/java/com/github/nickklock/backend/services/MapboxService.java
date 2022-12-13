e8c9b754a7d63ea744fa09c7feb5a3650a093c47
nick <klockgether.nick@gmail.com>
2022-12-13 22:29:06 +0100

    Add mapbox token to env variable, Add MapboxService to get the env variable

---
+++ backend/src/main/java/com/github/nickklock/backend/services/MapboxService.java
@@ -0,0 +1,14 @@
+package com.github.nickklock.backend.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MapboxService {
+    @Value("${mapbox.token}" )
+    private String token;
+
+    public String getToken(){
+        return token;
+    }
+}
