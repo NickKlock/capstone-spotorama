dd37629ecd31dd778753f0d5eeb4d0dfe185501d
nick <klockgether.nick@gmail.com>
2022-12-14 14:12:19 +0100

    Add Browser Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import useAccessToken from "./hooks/useAccessToken";
 import Homepage from "./components/Homepage";
+import {BrowserRouter, Route} from "react-router-dom";
+import AddSpot from "./components/AddSpot";
 
 function App() {
 
-  const {token} = useAccessToken()
+    const {token} = useAccessToken()
 
-  return (
-      <Homepage token={token}/>
-  )
+    return (
+        <BrowserRouter>
+            <Route element={<Homepage token={token}/>} path={"/"}/>
+            <Route element={<AddSpot/>} path={"/add-spot"}/>
+        </BrowserRouter>
+    )
 }
 
 export default App;
