c2acdecbe1f5d9972f789d0a59a5f5bcae9e3e1d
nick <klockgether.nick@gmail.com>
2022-12-14 16:42:05 +0100

    Create Statics file to store data

---
+++ frontend/src/statics/Statics.ts
@@ -0,0 +1,58 @@
+export const disciplines:string[] = [
+    "kitesurfing",
+    "wingsurfing",
+    "windsurfing",
+    "surfing"
+]
+
+export const wavetypes:string[]= [
+    "chop",
+    "big",
+    "flat",
+    "medium",
+    "small"
+]
+
+export const beachtypes:string[]= [
+    "sand",
+    "rocky",
+    "grass"
+]
+
+export const experiencelevel:string[]= [
+    "beginner",
+    "advanced",
+    "expert"
+]
+
+export const hazzards:string[]= [
+    "currents",
+    "rocks",
+    "reef"
+]
+
+export const bestmonths:string[]= [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+export const bestdirections:string[]= [
+    "N",
+    "S",
+    "W",
+    "O",
+    "NW",
+    "NE",
+    "SW",
+    "SE"
+]
