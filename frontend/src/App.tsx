c13215ab5ea755be5c234254eccb44e1f205005f
nick <klockgether.nick@gmail.com>
2022-12-14 11:53:06 +0100

    Add Homepage Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Map from "./components/Map";
 import useAccessToken from "./hooks/useAccessToken";
+import Homepage from "./components/Homepage";
 
 function App() {
 
   const {token} = useAccessToken()
 
   return (
-      <Map token={token}/>
+      <Homepage token={token}/>
   )
 }
 
---
+++ frontend/src/components/Homepage.tsx
@@ -0,0 +1,14 @@
+import {Box} from "@mui/material";
+import Map from "./Map";
+
+type HomepageProps = {
+    token:string
+}
+export default function Homepage(props:HomepageProps){
+
+    return(
+        <Box>
+            <Map token={props.token}/>
+        </Box>
+    )
+}
