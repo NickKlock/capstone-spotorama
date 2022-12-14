d2ec492d1a3dcdf68c026541027ba95a39135e24
nick <klockgether.nick@gmail.com>
2022-12-14 14:13:07 +0100

    Add navigate to AddSpot page

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -1,14 +1,19 @@
 import {Box, Fab} from "@mui/material";
 import Map from "./Map";
+import {useNavigate} from "react-router-dom";
 
 type HomepageProps = {
     token: string
 }
 export default function Homepage(props: HomepageProps) {
+    const navigate = useNavigate()
+    function handleAddButtonClick() {
+        navigate("/add-spot")
+    }
 
     return (
         <Box>
-            <Map token={props.token}/>
+            <Map handleAddButtonClick={handleAddButtonClick} token={props.token}/>
         </Box>
     )
 }
