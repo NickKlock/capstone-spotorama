27a2b2413fc218dd3dd5f03910a97411165934d2
nick <klockgether.nick@gmail.com>
2022-12-15 20:33:42 +0100

    Add integration test for post

---
+++ backend/src/test/java/com/github/nickklock/backend/controllers/SpotControllerTest.java
@@ -0,0 +1,45 @@
+package com.github.nickklock.backend.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class SpotControllerTest {
+
+    private final String endPoint = "/api/spots";
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void addSpot() throws Exception {
+        mvc.perform(post(endPoint)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"id":"1",
+                        "name":"test",
+                        "disciplines":["kitesurfing"],
+                        "wavetypes":["chop"],
+                        "beachtypes":["sand"],
+                        "experiencesLevel":["beginner"],
+                        "hazards":["currents"],
+                        "bestMonths":["May"],
+                        "bestDirections":["N"],
+                        "waterTemperature":["0-6"],
+                        "parkingSpace":0,
+                        "location":{"lat":54.7687425,"lng":9.9613969}
+                        }
+                        """)).andExpect(status().isCreated());
+    }
+}
