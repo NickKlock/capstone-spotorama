59498b4a5d5ff8310ea625617912614324fe5cac
nick <klockgether.nick@gmail.com>
2022-12-15 10:55:16 +0100

    Rename Enums to SpotSubTypes.ts cause I don't really need the enums

---
+++ frontend/src/models/SpotSubTypes.ts
@@ -0,0 +1,23 @@
+export type Disciplines = "kitesurfing" | "wingsurfing" | "windsurfing" | "surfing"
+
+
+export type WaveType = "chop" | "big" | "flat" | "medium" | "small"
+
+
+export type BeachType = "sand" | "rocky" | "grass"
+
+
+export type ExperiencesLevel = "beginner" | "advanced" | "expert"
+
+
+export type Hazard = "currents" | "rocks" | "reef"
+
+
+export type BestMonth = "January" | "February" | "March" | "April" | "May" | "June" | "July" | "August"
+    | "September" | "October" | "November" | "December"
+
+
+export type BestDirection = "N" | "S" | "W" | "O" | "NW" | "NE" | "SW" | "SE"
+
+
+export type WaterTemperature = "0-6" | "6-12" | "12-18" | "18-24" | "24+"
