f43fc67dffd90f6ee4a6d4373cc7806735dfd8c4
nick <klockgether.nick@gmail.com>
2022-12-15 16:01:02 +0100

    Add more fields to Spot.ts

--- frontend/src/models/Spot.ts
+++ frontend/src/models/Spot.ts
@@ -1,13 +1,26 @@
-import {Beachtypes, BestDirections, BestMonths, Disciplines, Experienceslevel, Hazards, Wavetypes} from "./Enmus";
+import {
+    BeachType,
+    BestDirection,
+    BestMonth,
+    Disciplines,
+    ExperiencesLevel,
+    Hazard,
+    WaterTemperature,
+    WaveType
+} from "./SpotSubTypes";
+import {Position} from "./Position";
 
 export type Spot= {
     id:string
     name:string
     disciplines:Disciplines[]
-    wavetypes:Wavetypes[]
-    beachtypes:Beachtypes[]
-    experiencesLevel:Experienceslevel[]
-    hazards:Hazards[]
-    bestMonths:BestMonths[]
-    bestDirections:BestDirections[]
+    wavetypes:WaveType[]
+    beachtypes:BeachType[]
+    experiencesLevel:ExperiencesLevel[]
+    hazards:Hazard[]
+    bestMonths:BestMonth[]
+    bestDirections:BestDirection[]
+    waterTemperature: WaterTemperature[]
+    parkingSpace: 0|1|2
+    location:Position
 }
