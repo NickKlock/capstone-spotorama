1e9853fcee49b298bb9664321d83c059d366eb50
nick <klockgether.nick@gmail.com>
2022-12-16 15:14:27 +0100

    Update test to work with enmus

--- backend/src/test/java/com/github/nickklock/backend/services/SpotServiceTest.java
+++ backend/src/test/java/com/github/nickklock/backend/services/SpotServiceTest.java
@@ -3,6 +3,7 @@ package com.github.nickklock.backend.services;
 import com.github.nickklock.backend.models.Position;
 import com.github.nickklock.backend.models.Spot;
 import com.github.nickklock.backend.models.SpotRequest;
+import com.github.nickklock.backend.models.enums.ParkingSpace;
 import com.github.nickklock.backend.repos.SpotRepo;
 import org.junit.jupiter.api.Test;
 
@@ -22,12 +23,12 @@ class SpotServiceTest {
         SpotRequest spotRequest = new SpotRequest( "test", new ArrayList<>(),
                 new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
                 new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
-                1, new Position(0, 0),"Yes");
+                ParkingSpace.FEW, new Position(0, 0),"Yes");
 
         Spot givenSpot = new Spot( "0","test", new ArrayList<>(),
                 new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
                 new ArrayList<>(), new ArrayList<>(), new ArrayList<>(),
-                1, new Position(0, 0),"Yes");
+                ParkingSpace.FEW, new Position(0, 0),"Yes");
 
         when(spotRepo.save(givenSpot)).thenReturn(givenSpot);
         when(idService.generateId()).thenReturn("0");
