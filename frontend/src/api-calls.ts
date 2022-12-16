28be066a123b6832f0f3bfad34523f1cef2fe9e7
nick <klockgether.nick@gmail.com>
2022-12-16 08:46:37 +0100

    Add Api call in Homepage.tsx, Add @Document annotation to Spot.java

--- frontend/src/api-calls.ts
+++ frontend/src/api-calls.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
+import {Spot} from "./models/Spot";
 
 export function getAccessToken(){
     return axios.get("/api/mapbox")
+        .then(response => response.data)
+        .catch(console.error)
+}
+
+export function addSpot(newSpot: Spot){
+    return axios.post("api/spots",newSpot)
         .then(response => response.data)
         .catch(console.error)
 }
