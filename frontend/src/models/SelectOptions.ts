d5e2bf3361adde41d6c0911b782d0286221ad4c6
nick <klockgether.nick@gmail.com>
2022-12-16 15:12:00 +0100

    Convert all options to UPPERCASE

---
+++ frontend/src/models/SelectOptions.ts
@@ -0,0 +1,65 @@
+export const disciplines:string[] = [
+    "KITESURFING",
+    "WINGSURFING",
+    "WINDSURFING",
+    "SURFING"
+]
+
+export const wavetypes:string[]= [
+    "CHOP",
+    "BIG",
+    "FLAT",
+    "MEDIUM",
+    "SMALL"
+]
+
+export const beachtypes:string[]= [
+    "SAND",
+    "ROCKY",
+    "GRASS"
+]
+
+export const experiencesLevel:string[]= [
+    "BEGINNER",
+    "ADVANCED",
+    "EXPERT"
+]
+
+export const hazards:string[]= [
+    "CURRENTS",
+    "ROCKS",
+    "REEF"
+]
+
+export const months:string[]= [
+    "JANUARY",
+    "FEBRUARY",
+    "MARCH",
+    "APRIL",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUGUST",
+    "SEPTEMBER",
+    "OCTOBER",
+    "NOVEMBER",
+    "DECEMBER",
+]
+
+export const windDirections:string[]= [
+    "N",
+    "S",
+    "W",
+    "E",
+    "NW",
+    "NE",
+    "SW",
+    "SE"
+]
+export const waterTemperatures:string[]= [
+    "COLD",
+    "MILD",
+    "WARM",
+    "HOT",
+    "TROPICAL"
+]
