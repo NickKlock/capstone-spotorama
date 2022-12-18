5b0514c748c50dbcf5dcbcba04ff3805c45963cf
nick <klockgether.nick@gmail.com>
2022-12-18 15:40:05 +0100

    Update SpotPopover with more details and styling

--- frontend/src/components/SpotPopover.tsx
+++ frontend/src/components/SpotPopover.tsx
@@ -1,14 +1,28 @@
 import {Spot} from "../models/Spot";
-import {Box, Typography} from "@mui/material";
+import {Box, IconButton, Typography} from "@mui/material";
+import {ArrowForward, Explore, Kitesurfing, Waves} from "@mui/icons-material";
 
 type SpotPopoverProps={
     spot:Spot
+    handleNavigate(id:string):void
 }
 export default function SpotPopover(props:SpotPopoverProps){
 
+    function handleArrowButtonClick() {
+        props.handleNavigate(props.spot.id)
+    }
 
     return(<Box>
-            <Typography variant={"h3"}>{props.spot.name}</Typography>
-        </Box>
+            <Typography variant={"h6"} textAlign={"center"}>{props.spot.name}</Typography>
+            <Typography><Kitesurfing/> {props.spot.disciplines.join(", ").toLowerCase()}</Typography>
+            <Typography> <Explore/> {props.spot.bestDirections.join(", ")}</Typography>
+            <Typography><Waves/> {props.spot.waveTypes.join(", ")}</Typography>
+            <Box display={"flex"} justifyContent={"flex-end"} alignItems={"flex-end"}>
+                <IconButton onClick={handleArrowButtonClick}>
+                    <ArrowForward/>
+                </IconButton>
+            </Box>
+
+            </Box>
     )
 }
