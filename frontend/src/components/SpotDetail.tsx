4512a5aa5f0dbda210ba5955432a978ec840dec5
nick <klockgether.nick@gmail.com>
2022-12-19 16:00:57 +0100

    Add SpotDetail.tsx page

--- frontend/src/components/SpotDetail.tsx
+++ frontend/src/components/SpotDetail.tsx
@@ -1,6 +1,22 @@
 import {useParams} from "react-router-dom";
-import {Box, CircularProgress} from "@mui/material";
+import {
+    Box,
+    CircularProgress,
+    Stack,
+} from "@mui/material";
 import useSpot from "../hooks/useSpot";
+import CustomAccordion from "./ui/CustomAccordion";
+import {
+    Attractions,
+    BeachAccess,
+    CalendarMonth,
+    Explore,
+    LocalParking,
+    MilitaryTech,
+    PriorityHigh,
+    Waves, Wc
+} from "@mui/icons-material";
+
 
 export default function SpotDetail(){
     const {id} = useParams()
@@ -12,9 +28,20 @@ export default function SpotDetail(){
             </Box>
         )
     }else {
-        return(<Box>
+        return(<Stack direction={"column"} spacing={2}>
+                <CustomAccordion icon={<Attractions/>} mainContent={spot.disciplines} title={"Best for"}/>
+                <CustomAccordion icon={<Explore/>} mainContent={spot.bestDirections} title={"Best wind directions"}/>
+                <CustomAccordion icon={<Waves/>} mainContent={spot.waveTypes} title={"Wave conditions"}/>
+                <CustomAccordion icon={<MilitaryTech/>} mainContent={spot.experiencesLevel} title={"Riders level"}/>
+                <CustomAccordion icon={<BeachAccess/>} mainContent={spot.beachTypes} title={"Beach"}/>
+                <CustomAccordion icon={<PriorityHigh/>} mainContent={spot.hazards} title={"Hazards"}/>
+                <CustomAccordion icon={<CalendarMonth/>} mainContent={spot.bestMonths} title={"Best time"}/>
+                <CustomAccordion icon={<LocalParking/>} mainContent={spot.parkingSpace} title={"Available parking space"}/>
+                <CustomAccordion icon={<Wc/>} mainContent={spot.restrooms} title={"Restrooms"}/>
+            </Stack>
+        )
 
-        </Box>)
+
     }
 
 }
