beb93437af548677cc19a0315a29b8573aeb4731
nick <klockgether.nick@gmail.com>
2022-12-19 16:00:25 +0100

    Wrap Mui Accordion in CustomAccordion.tsx

---
+++ frontend/src/components/ui/CustomAccordion.tsx
@@ -0,0 +1,25 @@
+import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@mui/material";
+
+type CustomAccordionProps = {
+    mainContent: string | string[]
+    title: string
+    icon:any
+
+}
+export default function CustomAccordion(props: CustomAccordionProps) {
+    return (
+        <Accordion>
+            <AccordionSummary
+                expandIcon={props.icon}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+            >
+                <Typography>{props.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={{}}>
+                <Typography>{Array.isArray(props.mainContent) ? props.mainContent.join(", ").toLowerCase() :
+                    props.mainContent.toLowerCase()}</Typography>
+            </AccordionDetails>
+        </Accordion>
+    )
+}
