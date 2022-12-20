5edbdb27587f8603ca84284af0e9a635e602cfa7
nick <klockgether.nick@gmail.com>
2022-12-20 10:47:45 +0100

    Add SpotList and SpotItem

---
+++ frontend/src/components/spot_list/SpotItem.tsx
@@ -0,0 +1,23 @@
+import {Spot} from "../../models/Spot";
+
+import {useNavigate} from "react-router-dom";
+import CustomCard from "../ui/CustomCard";
+
+type SpotItemProps = {
+    spot: Spot
+}
+export default function SpotItem(props: SpotItemProps) {
+    const navigate = useNavigate()
+
+    function handleGoToDetails() {
+        navigate("/spots/" + props.spot.id + "/details")
+    }
+
+    return (
+            <CustomCard
+                onClick={handleGoToDetails}
+                subtitle={props.spot.disciplines.join(", ").toLowerCase()}
+                avatarLetter={props.spot.name.charAt(0)}
+                title={props.spot.name}/>
+    )
+}
