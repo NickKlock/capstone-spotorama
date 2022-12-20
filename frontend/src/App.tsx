fb8dc5089efbc9b4be5c0cbc1e1ad6f55d16e7ec
nick <klockgether.nick@gmail.com>
2022-12-20 10:48:09 +0100

    Move fetching of all Spots to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import useAccessToken from "./hooks/useAccessToken";
 import Homepage from "./components/Homepage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SpotDetail from "./components/SpotDetail";
+import useSpots from "./hooks/useSpots";
+import SpotList from "./components/spot_list/SpotList";
 
 function App() {
 
     const {mapboxToken} = useAccessToken()
-
+    const {spots} = useSpots()
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Homepage mapboxToken={mapboxToken}/>} path={"/"}/>
+                <Route element={<Homepage spots={spots} mapboxToken={mapboxToken}/>} path={"/"}/>
+                <Route element={<SpotList spots={spots}/>} path={"/spots"}/>
                 <Route element={<SpotDetail/>} path={"/spots/:id/details"}/>
             </Routes>
         </BrowserRouter>
