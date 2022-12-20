786429b8bb4544b5e4f5243113932ee0ecfed64f
nick <klockgether.nick@gmail.com>
2022-12-20 10:58:14 +0100

    Add SearchInput in SpotList.tsx

--- frontend/src/components/spot_list/SpotList.tsx
+++ frontend/src/components/spot_list/SpotList.tsx
@@ -1,14 +1,25 @@
 import {Spot} from "../../models/Spot";
 import SpotItem from "./SpotItem";
-import {Stack} from "@mui/material";
+import {Stack, TextField} from "@mui/material";
+import {ChangeEvent, useState} from "react";
 
 type SpotListProps = {
-    spots:Spot[]
+    spots: Spot[]
 }
-export default function SpotList(props:SpotListProps){
+export default function SpotList(props: SpotListProps) {
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    function handleSearchInputChange(event:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setSearchTerm(event.target.value)
+    }
 
-    return(<Stack direction={"column"} spacing={2}  marginTop={2} >
-        {props.spots.map((spot) => <SpotItem spot={spot} key={spot.id}/>
+    const filteredSpots = props.spots.filter((spot) => spot.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    return (<Stack
+        direction={"column"}
+        spacing={2}
+        marginTop={2}>
+        <TextField type={"search"} onChange={handleSearchInputChange} placeholder={"Search for a Spot"}/>
+        {filteredSpots.map((spot) => <SpotItem spot={spot} key={spot.id}/>
         )}
     </Stack>)
 }
