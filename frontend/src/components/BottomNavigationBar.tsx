89a22efd0a3319c3f12f51155dd22c1ab7400896
nick <klockgether.nick@gmail.com>
2022-12-21 15:39:54 +0100

    Rename function

--- frontend/src/components/BottomNavigationBar.tsx
+++ frontend/src/components/BottomNavigationBar.tsx
@@ -8,13 +8,13 @@ export default function BottomNavigationBar(){
     const [currentPath, setCurrentPath] = useState<string>(pathname)
 
 
-    function handleChange(event: SyntheticEvent<Element, Event>, newValue: string) {
+    function handlePatchChange(event: SyntheticEvent<Element, Event>, newValue: string) {
         setCurrentPath(newValue)
     }
 
     return(<Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
             <Box>
-                <BottomNavigation value={currentPath} onChange={handleChange}>
+                <BottomNavigation value={currentPath} onChange={handlePatchChange}>
                     <BottomNavigationAction component={Link} to={'/'} value={'/'} label={'Map'} icon={<MapSharp/>} />
                     <BottomNavigationAction component={Link} to={'/spots'} value={'/spots'} label={'Search'} icon={<Search/>} />
                 </BottomNavigation>
