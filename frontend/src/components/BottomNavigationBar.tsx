18bf8449e72297a3b169ad044e6319e62e072cce
nick <klockgether.nick@gmail.com>
2022-12-21 15:21:08 +0100

    Update variable names and types

--- frontend/src/components/BottomNavigationBar.tsx
+++ frontend/src/components/BottomNavigationBar.tsx
@@ -5,16 +5,16 @@ import {MapSharp, Search} from "@mui/icons-material";
 
 export default function BottomNavigationBar(){
     const pathname = useLocation().pathname
-    const [value, setValue] = useState(pathname)
+    const [currentPath, setCurrentPath] = useState<string>(pathname)
 
 
-    function onChange(event: SyntheticEvent<Element, Event>, newValue: any) {
-        setValue(newValue)
+    function handleChange(event: SyntheticEvent<Element, Event>, newValue: string) {
+        setCurrentPath(newValue)
     }
 
     return(<Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
             <Box>
-                <BottomNavigation value={value} onChange={onChange}>
+                <BottomNavigation value={currentPath} onChange={handleChange}>
                     <BottomNavigationAction component={Link} to={'/'} value={'/'} label={'Map'} icon={<MapSharp/>} />
                     <BottomNavigationAction component={Link} to={'/spots'} value={'/spots'} label={'Search'} icon={<Search/>} />
                 </BottomNavigation>
