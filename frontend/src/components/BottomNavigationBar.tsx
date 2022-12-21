c66352d190ed84c3ceb977ba41b339f7865e6e46
nick <klockgether.nick@gmail.com>
2022-12-21 17:57:00 +0100

    Update BottomNavigation still broke

--- frontend/src/components/BottomNavigationBar.tsx
+++ frontend/src/components/BottomNavigationBar.tsx
@@ -1,4 +1,4 @@
-import {BottomNavigation, BottomNavigationAction, Paper} from "@mui/material";
+import {BottomNavigation, BottomNavigationAction, Box, Paper} from "@mui/material";
 import {Link, useLocation} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {MapSharp, Search} from "@mui/icons-material";
@@ -13,11 +13,14 @@ export default function BottomNavigationBar() {
 
 
     return (<Paper sx={{position: 'fixed', height: 56, bottom: 0, left: 0, right: 0}} elevation={3}>
-            <BottomNavigation value={currentPath}>
-                <BottomNavigationAction component={Link} to={'/'} value={'/'} label={'Map'} icon={<MapSharp/>}/>
-                <BottomNavigationAction component={Link} to={'/spots'} value={'/spots'} label={'Search'}
-                                        icon={<Search/>}/>
-            </BottomNavigation>
+            <Box>
+                <BottomNavigation value={currentPath}>
+                    <BottomNavigationAction component={Link} to={'/'} value={'/'} label={'Map'} icon={<MapSharp/>}/>
+                    <BottomNavigationAction component={Link} to={'/spots'} value={'/spots'} label={'Search'}
+                                            icon={<Search/>}/>
+                </BottomNavigation>
+            </Box>
+
         </Paper>
 
 
