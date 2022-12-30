e80f168d49cc721e9f0343fc71dca269cdb0759d
nick <klockgether.nick@gmail.com>
2022-12-30 15:33:15 +0100

    Update SpotList to be searchable again (only Country names for now)

--- frontend/src/components/spot_list/SpotList.tsx
+++ frontend/src/components/spot_list/SpotList.tsx
@@ -1,7 +1,7 @@
 import {Spot} from "../../models/Spot";
 import SpotItem from "./SpotItem";
 import {Accordion, AccordionDetails, AccordionSummary, Stack, TextField, Typography} from "@mui/material";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {ExpandMore} from "@mui/icons-material";
 
 type SpotListProps = {
@@ -9,53 +9,63 @@ type SpotListProps = {
 }
 export default function SpotList(props: SpotListProps) {
     const [searchTerm, setSearchTerm] = useState<string>("")
+    const [groupedSpots, setGroupedSpots] = useState<Map<string, Spot[]>>()
 
     function handleSearchInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         setSearchTerm(event.target.value.toLowerCase())
     }
 
-
-    const groupedSpots = new Map<string, Spot[]>()
-
-    props.spots.forEach((spot) => {
-        const countryName = spot.position.country
-        if (countryName) {
-            if (!groupedSpots.has(countryName)) {
-                groupedSpots.set(countryName, [])
+    useEffect(() => {
+        const mappedSpots = new Map<string, Spot[]>()
+        props.spots.forEach((spot) => {
+                if (!groupedSpots) {
+                    setGroupedSpots(new Map<string, Spot[]>())
+                }
+                const countryName = spot.position.country
+                if (countryName) {
+                    if (!mappedSpots.has(countryName)) {
+                        mappedSpots.set(countryName, [])
+                    }
+                    const spotsFromMap = mappedSpots.get(countryName)
+                    if (spotsFromMap) {
+                        spotsFromMap.push(spot)
+                    }
+                }
             }
-            const spotsFromMap = groupedSpots.get(countryName)
-            if (spotsFromMap) {
-                spotsFromMap.push(spot)
-            }
-        }
-    })
+        )
+        setGroupedSpots(mappedSpots)
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.spots])
+
+    if (groupedSpots) {
+        const filteredCountries = Array.from(groupedSpots).filter(([countryName]) => countryName.toLowerCase().includes(searchTerm))
+
+        return (
+            <Stack
+                direction={"column"}
+                spacing={2}
+                marginTop={10}>
+                <TextField type={"search"} onChange={handleSearchInputChange} placeholder={"Search for a country"}/>
+
+                {filteredCountries.map(([countryName, spots]) =>
+                    <Accordion key={countryName}>
+                        <AccordionSummary expandIcon={<ExpandMore/>}>
+                            <Typography>{countryName}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Stack direction={"column"} spacing={1}>
+                                {spots.map((spot) => <SpotItem spot={spot} key={spot.id}/>)}
+                            </Stack>
+                        </AccordionDetails>
+                    </Accordion>)}
+            </Stack>)
+    } else {
+
+        return (
+            <p>loading...</p>
+        )
+    }
 
 
-    const filteredSpots = Array.from(groupedSpots).filter(([countryName, spots]) => {
-        return countryName.toLowerCase().includes(searchTerm) || spots.filter((spot) => spot.name.toLowerCase().includes(searchTerm) ||
-            spot.disciplines.filter((discipline) => discipline.toLowerCase().includes(searchTerm)));
-    })
-
-    console.log(filteredSpots)
-
-
-    return (
-        <Stack
-            direction={"column"}
-            spacing={2}
-            marginTop={10}>
-            <TextField type={"search"} onChange={handleSearchInputChange} placeholder={"Search for a Spot"}/>
-
-            {filteredSpots.map(([countryName, spots]) =>
-                <Accordion key={countryName}>
-                    <AccordionSummary expandIcon={<ExpandMore/>}>
-                        <Typography>{countryName}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Stack direction={"column"} spacing={1}>
-                            {spots.map((spot) => <SpotItem spot={spot} key={spot.id}/>)}
-                        </Stack>
-                    </AccordionDetails>
-                </Accordion>)}
-        </Stack>)
 }
