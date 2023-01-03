0c71488e56837cde1694456f7e90e27af4978b4f
nick <klockgether.nick@gmail.com>
2023-01-03 12:45:39 +0100

    Implement UserDetailsService in UserService

--- backend/src/main/java/com/github/nickklock/backend/services/UserService.java
+++ backend/src/main/java/com/github/nickklock/backend/services/UserService.java
@@ -1,15 +1,21 @@
 package com.github.nickklock.backend.services;
 
+import com.github.nickklock.backend.exceptions.MyUsernameNotFoundException;
 import com.github.nickklock.backend.models.user.User;
 import com.github.nickklock.backend.models.user.UserAuth;
 import com.github.nickklock.backend.models.user.UserSpot;
 import com.github.nickklock.backend.repos.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepo userRepo;
     private final IdService idService;
     private final PasswordEncoder passwordEncoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
@@ -27,5 +33,12 @@ public class UserService {
                 userAuth.author()));
 
         return new UserSpot(savedUser.id(), savedUser.username(), savedUser.author());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(MyUsernameNotFoundException::new);
+        return new org.springframework.security.core.userdetails.User(user.username(), user.password(), List.of());
     }
 }
