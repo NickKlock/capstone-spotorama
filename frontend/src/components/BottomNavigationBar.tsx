eee17f9dd753c2596ef1bba755c4f17f074e5537
nick <klockgether.nick@gmail.com>
2023-01-03 17:26:30 +0100

    Add profile page to BottomNavigationBar.tsx

--- frontend/src/components/BottomNavigationBar.tsx
+++ frontend/src/components/BottomNavigationBar.tsx
@@ -1,23 +1,37 @@
 import {BottomNavigation, BottomNavigationAction, Box, Paper} from "@mui/material";
 import {Link, useLocation} from "react-router-dom";
 import {useEffect, useState} from "react";
-import {MapSharp, Search} from "@mui/icons-material";
+import {AccountCircle, MapSharp, Search} from "@mui/icons-material";
 
 export default function BottomNavigationBar() {
     const pathname = useLocation().pathname
     const [currentPath, setCurrentPath] = useState<string>(pathname)
 
-    useEffect(()=>{
+    useEffect(() => {
         setCurrentPath(pathname)
-    },[pathname])
+    }, [pathname])
 
 
     return (<Paper sx={{position: 'fixed', height: 56, bottom: 0, left: 0, right: 0}} elevation={3}>
             <Box>
                 <BottomNavigation value={currentPath}>
-                    <BottomNavigationAction component={Link} to={'/'} value={'/'} label={'Map'} icon={<MapSharp/>}/>
-                    <BottomNavigationAction component={Link} to={'/spots'} value={'/spots'} label={'Search'}
+                    <BottomNavigationAction component={Link}
+                                            to={'/'}
+                                            value={'/'}
+                                            label={'Map'}
+                                            icon={<MapSharp/>}/>
+
+                    <BottomNavigationAction component={Link}
+                                            to={'/spots'}
+                                            value={'/spots'}
+                                            label={'Search'}
                                             icon={<Search/>}/>
+
+                    <BottomNavigationAction component={Link}
+                                            to={'/profile'}
+                                            value={'/profile'}
+                                            label={'Profile'}
+                                            icon={<AccountCircle/>}/>
                 </BottomNavigation>
             </Box>
 
