4448f07ef4cef5c46df8c98ac40296740cfe3d80
nick <klockgether.nick@gmail.com>
2023-01-03 17:25:05 +0100

    Add api-call to register new User

--- frontend/src/api-calls.ts
+++ frontend/src/api-calls.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {Spot} from "./models/Spot";
+import {UserAuth} from "./models/User";
 
 export function getAccessToken(){
     return axios.get("/api/mapbox")
@@ -8,19 +9,25 @@ export function getAccessToken(){
 }
 
 export function postSpot(newSpot: Spot){
-    return axios.post("api/spots",newSpot)
+    return axios.post("/api/spots", newSpot)
         .then(response => response.data)
         .catch(console.error)
 }
 
 export function getAllSpots(){
-    return axios.get("api/spots")
+    return axios.get("/api/spots")
         .then(response => response.data)
         .catch(console.error)
 }
 
-export function getSpotById(id:string){
-    return axios.get("/api/spots/"+id)
+export function getSpotById(id: string) {
+    return axios.get("/api/spots/" + id)
+        .then(response => response.data)
+        .catch(console.error)
+}
+
+export function addUser(newUser: UserAuth) {
+    return axios.post("/api/users/", newUser)
         .then(response => response.data)
         .catch(console.error)
 }
