68693048da2cb0132021d30b2d964ba3644ab706
nick <klockgether.nick@gmail.com>
2023-01-03 12:44:22 +0100

    Add UserController and UserRepo

---
+++ backend/src/main/java/com/github/nickklock/backend/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.github.nickklock.backend.controllers;
+
+import com.github.nickklock.backend.models.user.UserAuth;
+import com.github.nickklock.backend.models.user.UserSpot;
+import com.github.nickklock.backend.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserSpot add(@RequestBody UserAuth userAuth) {
+        return userService.createNewUser(userAuth);
+    }
+}
--- backend/src/main/java/com/github/nickklock/backend/repos/UserRepo.java
+++ backend/src/main/java/com/github/nickklock/backend/repos/UserRepo.java
@@ -3,5 +3,8 @@ package com.github.nickklock.backend.repos;
 import com.github.nickklock.backend.models.user.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
