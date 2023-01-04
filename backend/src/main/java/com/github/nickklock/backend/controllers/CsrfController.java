f2e7b11721f00c9f9e004e29bac4e64e7fc9425c
nick <klockgether.nick@gmail.com>
2023-01-04 17:21:47 +0100

    Fix Security Hotspot

--- backend/src/main/java/com/github/nickklock/backend/controllers/CsrfController.java
+++ backend/src/main/java/com/github/nickklock/backend/controllers/CsrfController.java
@@ -2,14 +2,14 @@ package com.github.nickklock.backend.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CsrfController {
 
     @PreAuthorize("#request.getRemoteAddr().equals(#request.getLocalAddr())")
-    @RequestMapping("/csrf")
+    @GetMapping("/csrf")
     public CsrfToken csrf(CsrfToken token) {
         return token;
     }
