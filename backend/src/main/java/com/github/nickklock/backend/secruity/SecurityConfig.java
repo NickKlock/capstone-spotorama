480c243a6245bc0bac276f9cc512b7028c7a4889
nick <klockgether.nick@gmail.com>
2023-01-04 13:29:31 +0100

    Update csrf config to accept raw cookies, Add controller to get an csrf token

--- backend/src/main/java/com/github/nickklock/backend/secruity/SecurityConfig.java
+++ backend/src/main/java/com/github/nickklock/backend/secruity/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 
 @Configuration
@@ -23,19 +25,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        CookieCsrfTokenRepository tokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+        XorCsrfTokenRequestAttributeHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+        delegate.setCsrfRequestAttributeName("_csrf");
+        CsrfTokenRequestHandler requestHandler = delegate::handle;
+
         return httpSecurity
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(tokenRepository)
+                        .csrfTokenRequestHandler(requestHandler))
                 .httpBasic()
                 .authenticationEntryPoint((request, response, authException) ->
                         response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()))
                 .and()
                 .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers("/api/users/login/me").permitAll()
-                        .requestMatchers("/api/users/").permitAll()
+                        .requestMatchers("/api/users/me").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/users/").permitAll()
                         //.requestMatchers(HttpMethod.POST, "/api/users/login/").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/spots/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/csrf").permitAll()
                         //.requestMatchers(HttpMethod.DELETE, "/api/user/").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/spots/").authenticated()
                         .anyRequest().authenticated()
