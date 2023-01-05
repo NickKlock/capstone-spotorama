45c2ce077490adff421a118d72f1a23077ea117e
nick <klockgether.nick@gmail.com>
2023-01-05 14:55:53 +0100

    Add login page

--- frontend/src/components/profile/Login.tsx
+++ frontend/src/components/profile/Login.tsx
@@ -1,13 +1,55 @@
-import {Box} from "@mui/material";
-import {UserLoginRequest} from "../../models/User";
+import {Box, IconButton, TextField, Typography} from "@mui/material";
+import {UserLoginRequest, UserSpot} from "../../models/User";
+import {LoginSharp} from "@mui/icons-material";
+import {ChangeEvent, useState} from "react";
+import {Navigate, useNavigate} from "react-router-dom";
 
 type LoginProps = {
-    handleLoginRequest(userLoginRequest: UserLoginRequest): void
+    handleLoginRequest(userLoginRequest: UserLoginRequest): Promise<void>
+    loggedInUser: UserSpot
 }
 export default function Login(props: LoginProps) {
+    const navigate = useNavigate()
 
+    const [userLoginRequest, setUserLoginRequest] = useState<UserLoginRequest>(
+        {
+            username: "",
+            password: ""
+        })
 
-    return (<Box>
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setUserLoginRequest({...userLoginRequest, [event.target.name]: event.target.value})
+    }
 
-    </Box>)
+    function handleLogin() {
+        props.handleLoginRequest(userLoginRequest).then(() => navigate("/"))
+    }
+
+    return (
+        props.loggedInUser.username === "anonymousUser" ?
+            <Box flexDirection={"column"}
+                 display={"grid"}
+                 flexWrap={"wrap"}
+                 justifyContent={"center"}
+                 marginTop={"60px"}>
+
+                <Typography variant={"h4"} textAlign={"center"} sx={{mb: 5}}>Login</Typography>
+
+                <TextField name={"username"}
+                           onChange={handleInputChange}
+                           value={userLoginRequest.username}
+                           placeholder={"Hi, dude"}/>
+
+                <TextField name={"password"}
+                           type={"password"}
+                           onChange={handleInputChange}
+                           value={userLoginRequest.password}
+                           placeholder={"****"}/>
+
+                <IconButton onClick={handleLogin} color={"secondary"}>
+                    <LoginSharp/>
+                </IconButton>
+
+            </Box> : <Navigate to={"/"}/>
+    )
 }
