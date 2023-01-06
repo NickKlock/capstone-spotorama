41fbb3382001d897738f576042299ab6f97ededf
nick <klockgether.nick@gmail.com>
2023-01-06 10:40:55 +0100

    Create reusable UserForm

---
+++ frontend/src/components/profile/UserForm.tsx
@@ -0,0 +1,126 @@
+import {Box, Button, FormControl, List, ListItem, TextField, Typography} from "@mui/material";
+import CustomListItemTextInput from "../ui/CustomListItemTextInput";
+import {UserRequest, UserSpot} from "../../models/User";
+import {ChangeEvent, ReactNode, useState} from "react";
+
+type RegisterProps = {
+    loggedInUser: UserSpot
+    formTitle: string
+    buttonIcon: ReactNode
+    buttonText: string
+    color?: "secondary" | "success" | "inherit" | "warning" | "error" | "primary" | "info"
+    handleButtonClick(userRequestForm: UserRequest): void
+}
+
+export default function UserForm(props: RegisterProps) {
+    let authorInputFields = [
+        {
+            name: "nickname",
+            label: "Displayed name",
+            value: ""
+        },
+        {
+            name: "firstName",
+            label: "Name",
+            value: ""
+        },
+        {
+            name: "lastName",
+            label: "Last name",
+            value: ""
+        }
+    ]
+    let userRequest: UserRequest = {
+        author: {
+            createdSpots: [],
+            firstName: "",
+            lastName: "",
+            nickname: ""
+        },
+        password: "",
+        username: ""
+    }
+
+    if (props.loggedInUser.username !== "anonymousUser") {
+        userRequest.author = props.loggedInUser.author
+        userRequest.username = props.loggedInUser.username
+        userRequest.password = "********"
+
+        authorInputFields[0].value = userRequest.author.nickname
+        authorInputFields[1].value = userRequest.author.firstName
+        authorInputFields[2].value = userRequest.author.lastName
+    }
+
+    const [userRequestForm, setUserRequestForm] = useState<UserRequest>(userRequest)
+
+    function handleUserObjectInputChanges(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setUserRequestForm({...userRequestForm, [event.target.name]: event.target.value})
+    }
+
+    function handleAuthorObjectInputChanges(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setUserRequestForm(newUser => ({
+            ...newUser,
+            author: {
+                ...newUser.author,
+                [event.target.name]: event.target.value
+            }
+        }))
+    }
+
+
+    function handleRegisterUser() {
+        props.handleButtonClick(userRequestForm)
+    }
+
+    return (
+        <Box flexWrap={"wrap"}
+             display={"flex"}
+             justifyContent={"space-between"}
+             alignItems={"center"}
+             flexDirection={"column"}
+             marginTop={10}
+        >
+
+            <Typography textAlign={"center"} variant={"h6"}>{props.formTitle}</Typography>
+
+            <List>
+                <ListItem>
+                    <FormControl fullWidth={true} margin={"dense"}>
+                        <TextField name={"username"}
+                                   type={"email"}
+                                   label={"E-Mail"}
+                                   value={userRequestForm.username}
+                                   onChange={handleUserObjectInputChanges}/>
+                    </FormControl>
+                </ListItem>
+
+                <ListItem>
+                    <FormControl fullWidth={true} margin={"dense"}>
+                        <TextField name={"password"}
+                                   type={"password"}
+                                   label={"Password"}
+                                   value={userRequestForm.password}
+                                   onChange={handleUserObjectInputChanges}/>
+                    </FormControl>
+                </ListItem>
+
+                {authorInputFields.map((inputField) =>
+                    <CustomListItemTextInput name={inputField.name}
+                                             label={inputField.label}
+                                             onChange={handleAuthorObjectInputChanges}
+                                             value={inputField.value}
+                                             key={inputField.name}/>)}
+            </List>
+
+            <Button
+                color={props.color}
+                startIcon={props.buttonIcon}
+                variant={"contained"}
+                onClick={handleRegisterUser}
+            >
+                {props.buttonText}
+            </Button>
+
+        </Box>
+    )
+}
