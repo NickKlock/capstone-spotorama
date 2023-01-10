63ec97b7fb80c3759d1ade3a85c9dec44cc44eb0
nick <klockgether.nick@gmail.com>
2023-01-10 12:26:32 +0100

    Update: CustomAlert to have no close function

--- frontend/src/components/ui/CustomAlert.tsx
+++ frontend/src/components/ui/CustomAlert.tsx
@@ -1,22 +1,17 @@
 import {Alert, AlertColor, Fade, Snackbar} from "@mui/material";
-import {SyntheticEvent} from "react";
 
 type CustomAlertProps = {
-    handleClose(event?: SyntheticEvent | Event, reason?: string): void
     severity: AlertColor | undefined
     alertMessage: string
     open: boolean
 }
 export default function CustomAlert(props: CustomAlertProps) {
-    function handleClose(event?: SyntheticEvent | Event, reason?: string) {
-        props.handleClose(event, reason)
-    }
+
 
     return (
         <Snackbar open={props.open} autoHideDuration={6000}>
             <Fade>
-                <Alert onClose={handleClose}
-                       variant={"outlined"}
+                <Alert variant={"outlined"}
                        severity={props.severity}
                        sx={{width: "100%"}}>
                     {props.alertMessage}
