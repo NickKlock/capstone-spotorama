a46f5313a7d59ac968545430a03bd302061ddb2e
nick <klockgether.nick@gmail.com>
2023-01-10 12:40:30 +0100

    Update: ReAdd handleClose function so Alerts are actually closeable and close automatically

--- frontend/src/components/ui/CustomAlert.tsx
+++ frontend/src/components/ui/CustomAlert.tsx
@@ -4,15 +4,21 @@ type CustomAlertProps = {
     severity: AlertColor | undefined
     alertMessage: string
     open: boolean
+    onClose(): void
 }
 export default function CustomAlert(props: CustomAlertProps) {
-
+    function handleClose() {
+        props.onClose()
+    }
 
     return (
-        <Snackbar open={props.open} autoHideDuration={6000}>
+        <Snackbar open={props.open}
+                  onClose={handleClose}
+                  autoHideDuration={6000}>
             <Fade>
                 <Alert variant={"outlined"}
                        severity={props.severity}
+                       onClose={handleClose}
                        sx={{width: "100%"}}>
                     {props.alertMessage}
                 </Alert>
