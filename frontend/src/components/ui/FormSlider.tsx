758c1bd8b1749a8018036e4bd17f3596a089dfe8
nick <klockgether.nick@gmail.com>
2023-01-13 14:08:09 +0100

    Add: FormSlider Component

---
+++ frontend/src/components/ui/FormSlider.tsx
@@ -0,0 +1,39 @@
+import {Controller, RegisterOptions, useFormContext} from "react-hook-form";
+import {Box, FormHelperText, Slider} from "@mui/material";
+import {SpotFormInputs} from "../../models/FormInputTypes";
+
+type FormSliderProps = {
+    name: string
+    minValue: number
+    maxValue: number
+    defaultValue: number
+    step: number
+    marks: { value: number; label: string; }[]
+    rules?: RegisterOptions
+
+}
+export default function FormSlider(props: FormSliderProps) {
+    const {control, register} = useFormContext();
+    const userFormInputsKey = props.name as keyof SpotFormInputs
+
+    return (
+        <Controller render={({field: {onChange, value}, fieldState: {error}, formState}) =>
+            <Box marginTop={1} width={"100%"}>
+                <Slider
+                    {...register(props.name)}
+                    name={props.name}
+                    value={value}
+                    onChange={onChange}
+                    min={props.minValue}
+                    max={props.maxValue}
+                    defaultValue={props.defaultValue}
+                    step={props.step}
+                    valueLabelDisplay={"off"}
+                    marks={props.marks}/>
+                <FormHelperText>{error ? formState.errors[userFormInputsKey]?.message as string : null}</FormHelperText>
+            </Box>}
+                    rules={props.rules}
+                    control={control}
+                    name={props.name}/>
+    )
+}
