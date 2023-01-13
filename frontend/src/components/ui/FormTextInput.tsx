d3012ae6f1acf1d0c95154060f99c038a9862058
nick <klockgether.nick@gmail.com>
2023-01-13 09:00:01 +0100

    Update: FormTextInput to better re-usability

--- frontend/src/components/ui/FormTextInput.tsx
+++ frontend/src/components/ui/FormTextInput.tsx
@@ -1,11 +1,11 @@
-import {Control, Controller, RegisterOptions} from "react-hook-form";
+import {Controller, RegisterOptions, useFormContext} from "react-hook-form";
 import {TextField} from "@mui/material";
-import {UserFormInputs} from "../../models/UserFormInputs";
+import {UserFormInputs} from "../../models/FormInputTypes";
+import {Spot} from "../../models/Spot";
 
 interface FormTextInputProps {
     required: boolean;
-    name: "username" | "password" | "author" | "author.nickname" | "author.firstName" | "author.lastName" | "author.createdSpots"
-    control: Control<UserFormInputs>,
+    name: "username" | "password" | "author" | "author.nickname" | "author.firstName" | "author.lastName" | "author.createdSpots" | "name"
     label: string
     rules?: RegisterOptions
     editable: boolean
@@ -13,23 +13,28 @@ interface FormTextInputProps {
 }
 
 export default function FormTextInput(props: FormTextInputProps) {
-    const userFormInputsKey = props.name as keyof UserFormInputs
+    const {control, register} = useFormContext();
+
+    let userFormInputsKey = props.name as keyof UserFormInputs | Spot
+
     return (
         <Controller
             render={({field: {onChange, value}, fieldState: {error}, formState}) =>
                 <TextField
+                    {...register(props.name)}
                     label={props.label}
                     error={!!error}
                     onChange={onChange}
                     type={props.inputType}
                     value={value}
+                    // @ts-ignore
                     helperText={error ? formState.errors[userFormInputsKey]?.message as string : null}
                     disabled={!props.editable}
                     required={props.required}
                 />}
             name={props.name}
             rules={props.rules}
-            control={props.control}
+            control={control}
         />
     )
 }
