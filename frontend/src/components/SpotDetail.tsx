0f66caf58293faf3050e987993e5c8d46b940023
nick <klockgether.nick@gmail.com>
2023-01-14 09:50:14 +0100

    Update: SpotDetail to display the image if it exsist

--- frontend/src/components/SpotDetail.tsx
+++ frontend/src/components/SpotDetail.tsx
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom";
-import {Box, CircularProgress, Stack,} from "@mui/material";
+import {Box, Card, CardMedia, CircularProgress, Stack,} from "@mui/material";
 import useSpot from "../hooks/useSpot";
 import CustomAccordion from "./ui/custom-mui-components/CustomAccordion";
 import {
@@ -15,27 +15,36 @@ import {
 } from "@mui/icons-material";
 
 
-export default function SpotDetail(){
+export default function SpotDetail() {
     const {id} = useParams()
     const {spot} = useSpot(id)
 
-    if (!spot){
-        return (<Box display={"flex"} justifyContent={"center"} alignItems={"center"} margin={20} >
+
+    if (!spot) {
+        return (<Box display={"flex"} justifyContent={"center"} alignItems={"center"} margin={20}>
                 <CircularProgress/>
             </Box>
         )
-    }else {
-        return(<Stack direction={"column"} spacing={1} marginTop={10}>
-                <CustomAccordion icon={<Attractions/>} mainContent={spot.disciplines} title={"Best for"}/>
-                <CustomAccordion icon={<Explore/>} mainContent={spot.bestDirections} title={"Best wind directions"}/>
-                <CustomAccordion icon={<Waves/>} mainContent={spot.waveTypes} title={"Wave conditions"}/>
-                <CustomAccordion icon={<MilitaryTech/>} mainContent={spot.experiencesLevel} title={"Riders level"}/>
-                <CustomAccordion icon={<BeachAccess/>} mainContent={spot.beachTypes} title={"Beach"}/>
-                <CustomAccordion icon={<PriorityHigh/>} mainContent={spot.hazards} title={"Hazards"}/>
-                <CustomAccordion icon={<CalendarMonth/>} mainContent={spot.bestMonths} title={"Best time"}/>
-                <CustomAccordion icon={<LocalParking/>} mainContent={spot.parkingSpace} title={"Available parking space"}/>
-                <CustomAccordion icon={<Wc/>} mainContent={spot.restrooms} title={"Restrooms"}/>
-            </Stack>
+    } else {
+        return (
+            <Card sx={{marginTop: 10}}>
+                {spot.imageBase64Encoded &&
+                    <CardMedia component={"img"} src={`data:image/jpeg;base64,${spot.imageBase64Encoded}`}/>}
+                <Stack direction={"column"} spacing={1}>
+                    <CustomAccordion icon={<Attractions/>} mainContent={spot.disciplines} title={"Best for"}/>
+                    <CustomAccordion icon={<Explore/>} mainContent={spot.bestDirections}
+                                     title={"Best wind directions"}/>
+                    <CustomAccordion icon={<Waves/>} mainContent={spot.waveTypes} title={"Wave conditions"}/>
+                    <CustomAccordion icon={<MilitaryTech/>} mainContent={spot.experiencesLevel} title={"Riders level"}/>
+                    <CustomAccordion icon={<BeachAccess/>} mainContent={spot.beachTypes} title={"Beach"}/>
+                    <CustomAccordion icon={<PriorityHigh/>} mainContent={spot.hazards} title={"Hazards"}/>
+                    <CustomAccordion icon={<CalendarMonth/>} mainContent={spot.bestMonths} title={"Best time"}/>
+                    <CustomAccordion icon={<LocalParking/>} mainContent={spot.parkingSpace}
+                                     title={"Available parking space"}/>
+                    <CustomAccordion icon={<Wc/>} mainContent={spot.restrooms} title={"Restrooms"}/>
+                </Stack>
+            </Card>
+
         )
 
 
