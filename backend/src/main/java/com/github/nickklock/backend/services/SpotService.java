e64ba041c72e7138dff2a305177c0de5698777ea
nick <klockgether.nick@gmail.com>
2023-01-14 09:48:57 +0100

    Update: SpotService, using ImageService and String2Json service, Spot now gets saved with base64Encoded String

--- backend/src/main/java/com/github/nickklock/backend/services/SpotService.java
+++ backend/src/main/java/com/github/nickklock/backend/services/SpotService.java
@@ -1,23 +1,20 @@
 package com.github.nickklock.backend.services;
 
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.nickklock.backend.client.MapboxClient;
 import com.github.nickklock.backend.exceptions.NoSuchSpotException;
-import com.github.nickklock.backend.models.Photo;
 import com.github.nickklock.backend.models.Position;
 import com.github.nickklock.backend.models.Spot;
 import com.github.nickklock.backend.models.SpotRequest;
 import com.github.nickklock.backend.models.geocoding.CountryByCord;
 import com.github.nickklock.backend.repos.SpotRepo;
-import org.bson.types.Binary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
+@AllArgsConstructor
 @Service
 public class SpotService {
 
@@ -25,24 +22,37 @@ public class SpotService {
     private final IdService idService;
     private final MapboxClient mapboxClient;
     private final MapboxService mapboxService;
+    private final ImageService imageService;
+    private final String2JsonService string2JsonService;
 
-    public SpotService(SpotRepo spotRepo, IdService idService, MapboxClient mapboxClient, MapboxService mapboxService) {
-        this.spotRepo = spotRepo;
-        this.idService = idService;
-        this.mapboxClient = mapboxClient;
-        this.mapboxService = mapboxService;
-    }
+    public Spot add(String newSpotAsString, MultipartFile file) throws IOException {
+        SpotRequest newSpotRequest = string2JsonService.parseJsonToClass(newSpotAsString, SpotRequest.class);
 
-    public Spot add(SpotRequest newSpot) throws IOException {
-        Position positionFromRawData = createPositionFromRawData(newSpot.position().lng(), newSpot.position().lat());
+        SpotRequest spotRequestWithSpotImage = newSpotRequest.withSpotImage(file);
 
-        Photo photo = new Photo(newSpot.spotImage().getOriginalFilename(), new Binary(newSpot.spotImage().getBytes()));
-        Spot spotWithId = new Spot(idService.generateId(), newSpot.name(), newSpot.disciplines(), newSpot.waveTypes(),
-                newSpot.beachTypes(), newSpot.experiencesLevel(), newSpot.hazards(), newSpot.bestMonths(),
-                newSpot.bestDirections(), newSpot.waterTemperature(),
-                newSpot.parkingSpace(), positionFromRawData, newSpot.restrooms(), photo);
+        String spotImageBase64encoded = imageService.
+                encodeImageToBase64(spotRequestWithSpotImage.spotImage().getBytes());
+
+        Position positionFromRawData = createPositionFromRawData
+                (newSpotRequest.position().lng(), newSpotRequest.position().lat());
+
+        Spot spotWithId = new Spot(idService.generateId(),
+                spotRequestWithSpotImage.name(),
+                spotRequestWithSpotImage.disciplines(),
+                spotRequestWithSpotImage.waveTypes(),
+                spotRequestWithSpotImage.beachTypes(),
+                spotRequestWithSpotImage.experiencesLevel(),
+                spotRequestWithSpotImage.hazards(),
+                spotRequestWithSpotImage.bestMonths(),
+                spotRequestWithSpotImage.bestDirections(),
+                spotRequestWithSpotImage.waterTemperature(),
+                spotRequestWithSpotImage.parkingSpace(),
+                positionFromRawData,
+                spotRequestWithSpotImage.restrooms(),
+                spotImageBase64encoded);
 
         return spotRepo.save(spotWithId);
+
     }
 
     public List<Spot> list() {
@@ -61,34 +71,5 @@ public class SpotService {
         return new Position(lng, lat, getCountryByCords(lng, lat).features().get(0).text());
     }
 
-    public Spot add(String newSpotAsString, MultipartFile file) {
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().featuresToEnable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS).build();
-        try {
-            SpotRequest newSpot = objectMapper.readValue(newSpotAsString, SpotRequest.class);
-            SpotRequest spotRequestWithSpotImage = newSpot.withSpotImage(file);
 
-            Photo photo = new Photo(spotRequestWithSpotImage.spotImage().getOriginalFilename(),
-                    new Binary(spotRequestWithSpotImage.spotImage().getBytes()));
-            Position positionFromRawData = createPositionFromRawData(newSpot.position().lng(), newSpot.position().lat());
-
-            Spot spotWithId = new Spot(idService.generateId(),
-                    spotRequestWithSpotImage.name(),
-                    spotRequestWithSpotImage.disciplines(),
-                    spotRequestWithSpotImage.waveTypes(),
-                    spotRequestWithSpotImage.beachTypes(),
-                    spotRequestWithSpotImage.experiencesLevel(),
-                    spotRequestWithSpotImage.hazards(),
-                    spotRequestWithSpotImage.bestMonths(),
-                    spotRequestWithSpotImage.bestDirections(),
-                    spotRequestWithSpotImage.waterTemperature(),
-                    spotRequestWithSpotImage.parkingSpace(),
-                    positionFromRawData,
-                    spotRequestWithSpotImage.restrooms(),
-                    photo);
-
-            return spotRepo.save(spotWithId);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
