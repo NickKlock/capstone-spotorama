ecd20a53b809d85f2460c42acf3e7567cf3c76da
nick <klockgether.nick@gmail.com>
2023-01-18 11:31:53 +0100

    Add: Image to User Objects

--- backend/src/main/java/com/github/nickklock/backend/models/user/UserRequest.java
+++ backend/src/main/java/com/github/nickklock/backend/models/user/UserRequest.java
@@ -1,13 +1,19 @@
 package com.github.nickklock.backend.models.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.nickklock.backend.annotations.password.ValidPassword;
 import jakarta.validation.constraints.Email;
+import lombok.With;
+import org.springframework.web.multipart.MultipartFile;
 
+@With
 public record UserRequest(
         @Email
         String username,
         @ValidPassword
         String password,
-        Author author
+        Author author,
+        @JsonIgnore
+        MultipartFile avatar
 ) {
 }
