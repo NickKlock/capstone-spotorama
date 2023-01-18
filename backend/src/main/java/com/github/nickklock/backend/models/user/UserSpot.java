cc8ee93277db6b73e8813abaa4d7717425d9d533
nick <klockgether.nick@gmail.com>
2023-01-18 11:33:14 +0100

    Add: Image to User Spot Constructor

--- backend/src/main/java/com/github/nickklock/backend/models/user/UserSpot.java
+++ backend/src/main/java/com/github/nickklock/backend/models/user/UserSpot.java
@@ -6,9 +6,11 @@ public record UserSpot(
         @Id
         String id,
         String username,
-        Author author
+        Author author,
+        String avatarBase64Encoded
+
 ) {
-        public UserSpot(User user) {
-                this(user.id(), user.username(), user.author());
-        }
+    public UserSpot(User user) {
+        this(user.id(), user.username(), user.author(), user.avatarBase64Encoded());
+    }
 }
