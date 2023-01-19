ba42e566ebe0c4f4b2e78b6f5867c5822c9d7d73
nick <klockgether.nick@gmail.com>
2023-01-19 09:29:31 +0100

    Add: mui theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,47 +6,49 @@ import useSpots from "./hooks/useSpots";
 import SpotList from "./components/spot-list/SpotList";
 import BottomNavigationBar from "./components/BottomNavigationBar";
 import TitleBarRoutes from "./components/TitleBarRoutes";
-import {Box} from "@mui/material";
+import {CssBaseline, ThemeProvider} from "@mui/material";
 import Register from "./components/profile/Register";
 import useUser from "./hooks/useUser";
 import Login from "./components/profile/Login";
 import Profile from "./components/profile/Profile";
+import {muiTheme} from "./mui-theme";
 
 function App() {
     const {spots, addSpot} = useSpots()
     const {registerUser, loggedInUser, login, logout, edit, deletee} = useUser();
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Homepage handleAddSpot={addSpot}
-                                          spots={spots}/>}
-                       path={"/"}/>
+        <ThemeProvider theme={muiTheme}>
+            <CssBaseline/>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Homepage handleAddSpot={addSpot}
+                                              spots={spots}/>}
+                           path={"/"}/>
 
-
-                <Route element={<TitleBarRoutes/>}>
-                    <Route element={<Profile handleDeleteUser={deletee}
-                                             handleEditUser={edit}
-                                             handleLogout={logout}
-                                             loggedInUser={loggedInUser}/>}
-                           path={"/profile"}/>
-                    <Route element={<Login loggedInUser={loggedInUser}
-                                           handleLoginRequest={login}/>}
-                           path={"/login"}/>
-                    <Route element={<Register loggedInUser={loggedInUser}
-                                              handleRegisterUser={registerUser}/>}
-                           path={"/register"}/>
-                    <Route element={<SpotList spots={spots}/>}
-                           path={"/spots"}/>
-                    <Route element={<SpotDetail/>}
-                           path={"/spots/:id/details"}/>
-                </Route>
-            </Routes>
-
-            <Box height={56}>
+                    <Route element={<TitleBarRoutes/>}>
+                        <Route element={<Profile handleDeleteUser={deletee}
+                                                 handleEditUser={edit}
+                                                 handleLogout={logout}
+                                                 loggedInUser={loggedInUser}/>}
+                               path={"/profile"}/>
+                        <Route element={<Login loggedInUser={loggedInUser}
+                                               handleLoginRequest={login}/>}
+                               path={"/login"}/>
+                        <Route element={<Register loggedInUser={loggedInUser}
+                                                  handleRegisterUser={registerUser}/>}
+                               path={"/register"}/>
+                        <Route element={<SpotList spots={spots}/>}
+                               path={"/spots"}/>
+                        <Route element={<SpotDetail/>}
+                               path={"/spots/:id/details"}/>
+                    </Route>
+                </Routes>
                 <BottomNavigationBar/>
-            </Box>
-        </BrowserRouter>
+            </BrowserRouter>
+
+        </ThemeProvider>
+
     )
 }
 
