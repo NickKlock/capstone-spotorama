72513f535d63d002699471abf803b812a3fe556e
nick <klockgether.nick@gmail.com>
2023-01-23 23:34:52 +0100

    Update: Finding all Spots to return only minimal data to improve on loading times

--- backend/src/main/java/com/github/nickklock/backend/repos/SpotRepo.java
+++ backend/src/main/java/com/github/nickklock/backend/repos/SpotRepo.java
@@ -1,6 +1,7 @@
 package com.github.nickklock.backend.repos;
 
 import com.github.nickklock.backend.models.spot.Spot;
+import com.github.nickklock.backend.models.spot.SpotMinimal;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,5 @@ import java.util.List;
 @Repository
 public interface SpotRepo extends MongoRepository<Spot, String> {
     @Query("{'position.geo': {$geoWithin: { $centerSphere: [ [ ?0, ?1 ], ?2 ] } }}")
-    List<Spot> findByLocationWithin(double longitude, double latitude, double radiusInKilometer);
+    List<SpotMinimal> findByLocationWithin(double longitude, double latitude, double radiusInKilometer);
 }
