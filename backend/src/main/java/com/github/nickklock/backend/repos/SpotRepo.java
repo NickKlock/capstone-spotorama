142063878d9ae3bc581a568ab5630407120d12cb
nick <klockgether.nick@gmail.com>
2023-01-23 18:22:41 +0100

    Add: Custom Query to find Spots around a certain position

--- backend/src/main/java/com/github/nickklock/backend/repos/SpotRepo.java
+++ backend/src/main/java/com/github/nickklock/backend/repos/SpotRepo.java
@@ -2,8 +2,13 @@ package com.github.nickklock.backend.repos;
 
 import com.github.nickklock.backend.models.Spot;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SpotRepo extends MongoRepository<Spot,String> {
+public interface SpotRepo extends MongoRepository<Spot, String> {
+    @Query("{'position.geo': {$geoWithin: { $centerSphere: [ [ ?0, ?1 ], ?2 ] } }}")
+    List<Spot> findByLocationWithin(double longitude, double latitude, double radiusInKilometer);
 }
